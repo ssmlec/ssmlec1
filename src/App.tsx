94b82c47c69ab5498022b6834b08977e44ef0c03
ssmlec <ssmlec2023@gmail.com>
2026-08-13 17:36:09 +0530

    commented dark mode and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ export default function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <SSMLECChatbot />
-      {/* <MBGWebchatWidget/> */}
+      <MBGWebchatWidget/>
     </main>
 
     <Footer />
